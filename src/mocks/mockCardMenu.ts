173dda02f12bac039a7ab2b307212cb9c42013b8
Rafael Gomez <rafagomes10@live.com>
2025-04-02 12:36:25 -0300

    images cardMenu

--- src/mocks/mockCardMenu.ts
+++ src/mocks/mockCardMenu.ts
@@ -1,3 +1,5 @@
+
+
 export interface CardMenuType {
     id: number;
     nome: string;
@@ -10,36 +12,36 @@ export const mockCardMenu: CardMenuType[] = [
         id: 1,
         nome: "Menu Principal",
         cor: "#8B6144",
-        imagem: "/menu-principal.jpg"
+        imagem: "https://img.cdndsgni.com/preview/11099453.jpg"
     },
     {
         id: 2,
         nome: "Sushibar",
         cor: "#A64B2A",
-        imagem: "/sushibar.jpg"
+        imagem: "https://www.zapimoveis.com.br/blog/wp-content/uploads/2024/01/dia-do-sushi-no-brasil.jpg"
     },
     {
         id: 3,
         nome: "Carta de Vinhos",
         cor: "#722F37",
-        imagem: "/vinhos.jpg"
+        imagem: "https://www.divvino.com.br/blog/wp-content/uploads/2020/03/garrafas-de-vinho.jpg"
     },
     {
         id: 4,
         nome: "Carta de Sake",
         cor: "#C0C0C0",
-        imagem: "/sake.jpg"
+        imagem: "https://www.guiadasemana.com.br/contentFiles/system/pictures/2011/9/25280/original/garrafas-saque.jpg"
     },
     {
         id: 5,
         nome: "Bebidas",
         cor: "#2E8B57",
-        imagem: "/bebidas.jpg"
+        imagem: "https://img.freepik.com/fotos-premium/bebidas-refrigeradas-mezcla-sabores-experiencia-refrescante-concept-mocktail-recetas-bebidas-verano-bebidas-refrescantes-cocteles-refrigerados-bebidas-sabrosas_864588-54183.jpg"
     },
     {
         id: 6,
-        nome: "teste",
-        cor: "#f1f",
-        imagem: "/bebidas.jpg"
+        nome: "Teste",
+        cor: "orange",
+        imagem: "https://swiftbr.vteximg.com.br/arquivos/churrasco-completo-de-dia-dos-pais.jpg"
     }
 ];
